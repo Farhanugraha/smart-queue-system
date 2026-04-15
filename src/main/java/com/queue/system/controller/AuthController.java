fc57d847e31f6b680df01765f9f96288feeea22b
Farhan Nugraha <farhan.14092003@gmail.com>
2026-04-15 11:03:46 +0700

    create API response

---
+++ src/main/java/com/queue/system/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.queue.system.controller;
+
+
+import com.queue.system.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+}
